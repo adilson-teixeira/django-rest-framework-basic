441de27026fbb82446ca6c762d9f52feb88b4ce3
proj-int01 <adilson.monte.alto@gmail.com>
2022-07-30 20:13:55 -0300

    refatoração

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,16 @@
-from app.views import todo_list, todo_detail_change_and_delete
-from django.urls import path
+#from app.views import TodoListAndCreat, TodoDetailChangeAndDelete
+from app.views import TodoViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register(r'', TodoViewSet)
+
+urlpatterns = router.urls
+
+
+"""from django.urls import path
 
 urlpatterns = [
-    path('', todo_list),
-    path('<int:pk>/', todo_detail_change_and_delete),
-]
+    path('', TodoListAndCreat.as_view()),
+    path('<int:pk>/', TodoDetailChangeAndDelete.as_view()),
+]"""
--- app/views.py
+++ app/views.py
@@ -1,9 +1,44 @@
 from app.models import Todo
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework import viewsets
+from rest_framework import generics
 from app.serializers import TodoSerializers
 
+class TodoViewSet(viewsets.ModelViewSet):
+    """View reduzida criada com viewsets"""
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+
+
+"""class TodoListAndCreat(generics.ListCreateAPIView):
+    # Viwes com funções implementadas pela generics
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+
+class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
+     # Viwes com funções implementadas pela generics
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers"""
+
+"""class TodoListAndCreat(APIView):
+    #View refatorada em ClassBasedViews implementado as funções
+    def get(self, request):
+        todo = Todo.objects.all()
+        serializer = TodoSerializers(todo, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = TodoSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+function based views
 @api_view(['GET', 'POST'])
 def todo_list(request):
     if request.method == 'GET':
@@ -15,9 +50,39 @@ def todo_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)"""
+
+
+
+"""class TodoDetailChangeAndDelete(APIView):
+    #View refatorada em ClassBasedViews implementando as funções
+    def get_object(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            raise NotFound()
+
+    def get(self, request, pk):
+        todo = self.get_object(pk)
+        serializer = TodoSerializers(todo)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        todo = self.get_object(pk)
+        serializer =  TodoSerializers(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        todo = self.get_object(pk)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT) """
 
+
+
+""" function based views
 @api_view(['GET', 'PUT', 'DELETE'])
 def todo_detail_change_and_delete(request, pk):
     try:
@@ -36,4 +101,4 @@ def todo_detail_change_and_delete(request, pk):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         todo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)"""
